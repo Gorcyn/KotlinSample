a2431a21b256203a5de8deee987759fadac122a7
Ludovic Garcia <l.garcia@eutech-ssii.com>
2015-01-27 17:15:42 +0100

    Initial commit

---
+++ app/src/main/java/com/gorcyn/sample/kotlin/MainActivity.java
@@ -0,0 +1,24 @@
+package com.gorcyn.sample.kotlin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void startKotlinActivity(View view) {
+        Intent intent = new Intent(this, KotlinActivity.class);
+        this.startActivity(intent);
+    }
+}
---
+++ app/src/main/kotlin/com/gorcyn/sample/kotlin/KotlinActivity.kt
@@ -0,0 +1,42 @@
+package com.gorcyn.sample.kotlin
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.ActionBarActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
+import android.widget.TextView
+
+import java.text.DateFormat
+import java.util.Date
+import android.app.AlertDialog
+
+open class KotlinActivity : ActionBarActivity() {
+
+    protected override fun onCreate(savedInstanceState: Bundle?): Unit {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        val time: TextView = findViewById(R.id.time) as TextView
+        time.setText(DateFormat.getDateTimeInstance().format(Date()))
+    }
+
+    public fun finishActivity(view: View) {
+        when (view.getId()) {
+            R.id.finish, R.id.hello -> finish()
+            else -> alert("You clicked on time!")
+        }
+    }
+
+    fun alert(message: String) {
+        AlertDialog.Builder(this)
+            .setTitle(R.string.failure)
+            .setMessage(message)
+            .setCancelable(false)
+            .setPositiveButton(android.R.string.ok, null)
+            .show();
+    }
+}
